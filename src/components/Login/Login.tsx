2ad9aa8c60ee348231ad24b69293647d2252cd0c
LeeJaeSeoung <wotjd0676@gmail.com>
2022-04-03 16:57:01 +0900

    [Update] Router setting
    라우터 세팅

---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import * as S from "./styled";
+
+const Login = () => {
+  return (
+    <S.Container>
+      
+    </S.Container>
+  )
+}
+
+export default Login;
---
+++ src/route/MainRouter.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+import { Routes, Route } from "react-router-dom";
+import { LoginContainer } from "../container";
+
+const MainRouter: FC = (): JSX.Element => {
+  return (
+    <Routes>
+      <Route path="/" element={<LoginContainer />} />
+    </Routes>
+  );
+};
+
+export default MainRouter;
