69848d67e19f81a4e9fda700ae01386c86f43ca7
LeeJaeSeoung <wotjd0676@gmail.com>
2022-04-03 16:56:36 +0900

    [Update] Global Style
    기본 스타일 설정

---
+++ src/style/color.ts
@@ -0,0 +1,9 @@
+export const color = {
+  white: "#FFFFFF",
+  lightBlack: "#212121",
+  black: "#000000",
+  lightRed: "#FF6262",
+  lightGray: "#E0E0E0",
+  gray: "#9E9E9E",
+  darkGray: "#424242",
+};
