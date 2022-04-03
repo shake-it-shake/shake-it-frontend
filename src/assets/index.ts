7a14b7cd307fac7fea9ccc010d5060430f35f9b5
LeeJaeSeoung <wotjd0676@gmail.com>
2022-04-03 16:56:12 +0900

    [Update] Add assets
    svg파일 추가

---
+++ src/assets/index.ts
@@ -0,0 +1,7 @@
+export { default as plusIcon } from "./menu/plusIcon.svg";
+export { default as friendsIcon } from "./menu/friendsIcon.svg";
+export { default as profileIcon } from "./menu/profileIcon.svg";
+export { default as closeIcon } from "./closeIcon.svg";
+export { default as discoBacll } from "discoBall.svg";
+export { default as login } from "login.svg";
+export { default as searchIcon } from "searchIcon.svg";
