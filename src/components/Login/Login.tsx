595883229ef746c966f4eb7222cf0004d6314cab
LeeJaeSeoung <wotjd0676@gmail.com>
2022-04-04 10:22:10 +0900

    [Uodate] Login publising
    로그인 퍼블리싱 완료

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React from "react";
 import * as S from "./styled";
 
 const Login = () => {
+
   return (
     <S.Container>
-      
+      <S.LoginImg />
+      <S.LoginContainer>
+        <S.Title>로그인하고 흔들어보세요</S.Title>
+        <S.LoginInput name="Id" placeholder="아이디"/>
+        <S.LoginInput name="Pw" placeholder="비밀번호" type="password"/>
+        <S.LoginButton>로그인</S.LoginButton>
+        <S.Guide>
+          <S.GuideText>
+            계정이 없으신가요?
+          </S.GuideText>
+          <S.SignUpText>
+            회원가입 하기
+          </S.SignUpText>
+        </S.Guide>
+      </S.LoginContainer>
     </S.Container>
-  )
-}
+  );
+};
 
 export default Login;
--- src/components/Login/styled.ts
+++ src/components/Login/styled.ts
@@ -1,6 +1,75 @@
 import styled from "@emotion/styled";
+import { login } from "assets";
+import { color } from "style/color";
 
 export const Container = styled.div`
   width: 100vw;
   height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 180px;
+`
+
+export const LoginImg = styled.svg`
+  background-image: url(${login});
+  width: 516px;
+  height: 575px;
+`
+
+export const LoginContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+export const Title = styled.div`
+  font-size: 32px;
+  color: ${color.white};
+  margin-bottom: 62px;
+`
+
+export const LoginInput = styled.input`
+  margin-bottom: 12px;
+  padding: 13px 16px;
+  color: white;
+  font-size: 16px;
+  background-color: ${color.black};
+  border: solid 1px ${color.gray};
+  border-radius: 12px;
+  outline: none;
+  box-shadow: 0 0 1px 0 ${color.gray} inset, 0 0 1px 0 ${color.gray};
+  ::placeholder {
+    color: ${color.gray}
+  }
+`
+
+export const LoginButton = styled.button`
+  margin-top: 18px;
+  margin-bottom: 26px;
+  background-color: ${color.lightRed};
+  cursor: pointer;
+  display: flex;
+  padding: 13px 144px;
+  color: ${color.white};
+  font-size: 16px;
+  font-weight: bold;
+  border: none;
+  border-radius: 12px;
+`
+
+export const Guide = styled.div`
+  display: flex;
+  font-weight: 300;
+  font-size: 16px;
+
+`
+
+export const GuideText = styled.div`
+  color: ${color.white};
+  margin-right: 20px;
+`
+
+export const SignUpText = styled.div`
+  color: ${color.lightRed};
+  cursor: pointer;
 `
