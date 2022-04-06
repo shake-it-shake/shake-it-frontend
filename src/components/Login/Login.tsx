90a411b50673096e9be672d833f1909f56967da7
LeeJaeSeoung <wotjd0676@gmail.com>
2022-04-06 17:05:08 +0900

    [Update] Input, Modal LInk
    모달열 수 있게 활성화, 인풋값 state로 관리함

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,26 +1,33 @@
-import React from "react";
+import { Fragment } from "react";
+import { useNavigate } from "react-router-dom";
 import * as S from "./styled";
 
 const Login = () => {
+  const navigate = useNavigate();
 
   return (
-    <S.Container>
-      <S.LoginImg />
-      <S.LoginContainer>
-        <S.Title>로그인하고 흔들어보세요</S.Title>
-        <S.LoginInput name="Id" placeholder="아이디"/>
-        <S.LoginInput name="Pw" placeholder="비밀번호" type="password"/>
-        <S.LoginButton>로그인</S.LoginButton>
-        <S.Guide>
-          <S.GuideText>
-            계정이 없으신가요?
-          </S.GuideText>
-          <S.SignUpText>
-            회원가입 하기
-          </S.SignUpText>
-        </S.Guide>
-      </S.LoginContainer>
-    </S.Container>
+    <Fragment>
+      <S.Container>
+        <S.LoginImg />
+        <S.LoginContainer>
+          <S.Title>로그인하고 흔들어보세요</S.Title>
+          <S.LoginInput name="Id" placeholder="아이디" autoComplete="off" />
+          <S.LoginInput
+            name="Pw"
+            placeholder="비밀번호"
+            type="password"
+            autoComplete="off"
+          />
+          <S.LoginButton>로그인</S.LoginButton>
+          <S.Guide>
+            <S.GuideText>계정이 없으신가요?</S.GuideText>
+            <S.SignUpText onClick={() => navigate("/signup/info")}>
+              회원가입 하기
+            </S.SignUpText>
+          </S.Guide>
+        </S.LoginContainer>
+      </S.Container>
+    </Fragment>
   );
 };
 
