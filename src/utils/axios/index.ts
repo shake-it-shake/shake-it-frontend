6ce82fd066d03b52c747d538364d0504688afd5b
LeeJaeSeoung <wotjd0676@gmail.com>
2022-04-14 16:49:47 +0900

    [Update] add BASE_URL, refresh
    토큰 일단 대충 만듦

--- src/utils/axios/index.ts
+++ src/utils/axios/index.ts
@@ -1,17 +1,17 @@
 import axios, { AxiosError } from "axios";
-// import { refresh, refreshError } from "../refresh";
+import { refresh, refreshError } from "../refresh";
 
 export const request = axios.create({
-  baseURL: "",
+  baseURL: "http://13.124.183.39:3000/",
   timeout: 100000,
 });
 
 const instance = axios.create({
-  baseURL: "",
+  baseURL: "http://13.124.183.39:3000/",
   timeout: 100000,
 });
 
-// instance.interceptors.request.use(refresh, refreshError);
+instance.interceptors.request.use(refresh, refreshError);
 
 instance.interceptors.response.use(
   function (response) {
--- src/utils/refresh/index.ts
+++ src/utils/refresh/index.ts
@@ -1,6 +1,6 @@
-import axios, { AxiosRequestConfig } from "axios";
-// import uri from "../../constance/uri";
-// import { request } from "./axios";
+import { AxiosRequestConfig } from "axios";
+import uri from "../../constance/uri";
+import { request } from "../axios";
 
 interface TokenType {
   access_token: string,
@@ -8,61 +8,59 @@ interface TokenType {
   expired_at: string,
 }
 
-// const getDateWithAddHour = (hour: number) => {
-//   const date = new Date();
-//   date.setHours(date.getHours() + hour);
-//   return date;
-// };
+const getDateWithAddHour = (hour: number) => {
+  const date = new Date();
+  date.setHours(date.getHours() + hour);
+  return date;
+};
 
-// const refresh = async (config: AxiosRequestConfig): Promise<AxiosRequestConfig> => {
-//   const expireAt = localStorage.getItem("expireAt");
-//   let accessToken = localStorage.getItem("access_token");
-//   const refreshToken = localStorage.getItem("refresh_token");
+const refresh = async (config: AxiosRequestConfig): Promise<AxiosRequestConfig> => {
+  const expireAt = localStorage.getItem("expireAt");
+  const refreshToken = localStorage.getItem("refresh_token");
 
-//   if (!refreshToken || !expireAt) {
-//     //리프레시 토큰이 없으면 로그인 상태가 아님
-//     window.location.href = "/";
-//     localStorage.removeItem("expireAt");
-//     localStorage.removeItem("access_token");
-//     localStorage.removeItem("refresh_token");
+  if (!refreshToken || !expireAt) {
+    //리프레시 토큰이 없으면 로그인 상태가 아님
+    window.location.href = "/";
+    localStorage.removeItem("expireAt");
+    localStorage.removeItem("access_token");
+    localStorage.removeItem("refresh_token");
 
-//     return config;
-//   }
+    return config;
+  }
 
-//   if (new Date().getTime() > new Date(expireAt).getTime()) {
-//     //엑세스 토큰 만료
-//     const data = {
-//       refresh_token: refreshToken,
-//     };
+  if (new Date().getTime() > new Date(expireAt).getTime()) {
+    //엑세스 토큰 만료
+    const data = {
+      refresh_token: refreshToken,
+    };
 
-//     try {
-//       const { access_token, refresh_token } = (await request.post<TokenType>(uri.refresh, data))
-//         .data.content.response;
-//       accessToken = access_token;
+    try {
+      const { access_token, refresh_token } = (await request.put<TokenType>(uri.refresh, data))
+        .data;
 
-//       localStorage.setItem("access_token", access_token);
-//       localStorage.setItem("refresh_token", refresh_token);
-//       localStorage.setItem("expireAt", getDateWithAddHour(2).toString());
-//     } catch {
-//       //리프레시 토큰 만료
-//       window.location.href = "/";
-//       localStorage.removeItem("expireAt");
-//       localStorage.removeItem("access_token");
-//       localStorage.removeItem("refresh_token");
+      localStorage.setItem("access_token", access_token);
+      localStorage.setItem("refresh_token", refresh_token);
+      localStorage.setItem("expireAt", getDateWithAddHour(2).toString());
+    } catch {
+      //리프레시 토큰 만료
+      window.location.href = "/";
+      localStorage.removeItem("expireAt");
+      localStorage.removeItem("access_token");
+      localStorage.removeItem("refresh_token");
 
-//       return config;
-//     }
-//   }
+      return config;
+    }
+  }
 
-//   // config.headers["Authorization"] = `Bearer ${accessToken}`;
+  // config.headers["Authorization"] = `Bearer ${accessToken}`;
 
-//   return config;
-// };
+  return config;
+};
 
-// const refreshError = (err: any) => {
-//   localStorage.removeItem("expireAt");
-//   localStorage.removeItem("access_token");
-//   localStorage.removeItem("refresh_token");
-// };
+const refreshError = (err: any) => {
+  localStorage.removeItem("expireAt");
+  localStorage.removeItem("access_token");
+  localStorage.removeItem("refresh_token");
+};
 
-// export { refresh, refreshError };
+export { refresh, refreshError };
