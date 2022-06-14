67fd2abb6ee64fa803d9ca895d547c6879d99f6a
LeeJaeSeoung <wotjd0676@gmail.com>
2022-06-14 10:21:41 +0900

    [Update] Add Room Container
    컨테이너에 추가해서 경로설정

--- src/container/index.ts
+++ src/container/index.ts
@@ -1,2 +1,3 @@
 export { default as LoginContainer } from "./Login";
-export { default as MainContainer } from "./Main"
+export { default as MainContainer } from "./Main";
+export { default as RoomContainer } from "./Room";
--- src/route/MainRouter.tsx
+++ src/route/MainRouter.tsx
@@ -1,12 +1,13 @@
 import { FC } from "react";
 import { Routes, Route } from "react-router-dom";
-import { LoginContainer, MainContainer } from "../container";
+import { LoginContainer, MainContainer, RoomContainer } from "../container";
 
 const MainRouter: FC = (): JSX.Element => {
   return (
     <Routes>
       <Route path="/" element={<LoginContainer />} />
       <Route path="/main" element={<MainContainer />} />
+      <Route path="/room/:roomid" element={<RoomContainer />} />
     </Routes>
   );
 };
